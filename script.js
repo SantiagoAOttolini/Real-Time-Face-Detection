08b66b88082bf58d960b9c36aac15c2fd838babe
santiago <santiago.ottolini97@hotmail.com>
2021-05-04 15:52:36 -0300

    Add models folder from face-api

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const video = document.getElementById("video");
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+]).then(startVideo);
+
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
@@ -6,4 +13,7 @@ function startVideo() {
     (err) => console.log(err)
   );
 }
-startVideo();
+
+video.addEventListener("play", () => {
+  console.log("Wewe");
+});
